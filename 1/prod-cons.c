b63ed6488f18a00bd2d06ce72ebfb487d6d69cf6
Aravind G. Parasurama <aravindsasth@gmail.com>
2017-04-21 01:42:10 -0700

    Fleshed out producer

--- 1/prod-cons.c
+++ 1/prod-cons.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <semaphore.h>
 #include "mt19937ar.h"
 
 struct data {
@@ -7,13 +10,19 @@ struct data {
 	int hold_time;
 };
 
+#define BUFFER_SIZE 32
+struct data items[BUFFER_SIZE];
+pthread_mutex_t mutex;
+sem_t full, empty;
+int counter;
+
 bool isx86();
 int rand_num(int, int);
 void consumer();
 void producer();
 
 bool isx86() {
-	unsigned int eax = 0x01, ebc, ecx, edx;
+	unsigned int eax = 0x01, ebx, ecx, edx;
 	
 	__asm__ __volatile__(
 			"cpuid;"
@@ -45,16 +54,50 @@ int rand_num(int max, int min) {
 	return result;
 }
 
-int main(int argc, char* argv[]) {
-	int threads, i;
+void producer() {
+	while(1) {
+		int hold_time = rand_num(5, 0);
+		sleep(hold_time);
 
+		int value = rand_num(50000000, 0);
+
+		sem_wait(&empty);
+
+		pthread_mutex_lock(&mutex);
+		if (counter < BUFFER_SIZE){
+			items[counter].value = value;
+			items[counter].hold_time = hold_time;
+			counter++;
+		}
+		pthread_mutex_unlock(&mutex);
+
+		sem_post(&full);
+	}
+}
+
+void consumer() {
+	
+}
+
+int main(int argc, char* argv[]) {
+	counter = 0;
+	
 	if (argc < 1) {
 		printf("Usage: ./a.out [number of threads]");
 	}
-	threads = atoi(argv[1]);
+	int threads = atoi(argv[1]);
+	isx86();
 
+	pthread_t thread_array[2*threads];
 	
+	for (int i=0; i<threads; i++) {
+		pthread_create(&thread_array[i], NULL, consumer, NULL);
+		pthread_create(&thread_array[i+1], NULL, producer, NULL);
+	}
 
-	pthread_t consumer_process;
-	pthread_create(&consumer_process, NULL, (void *)consumer, (void *)NULL);
+	for (int i=0; i<(2*threads); i++) {
+		pthread_join(threads[i], NULL);
+	}
+
+	return 0;
 }
