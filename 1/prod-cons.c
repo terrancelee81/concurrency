37f790c97c2b3a4929ba102acfb634af9fabf30b
Aravind G. Parasurama <aravindsasth@gmail.com>
2017-04-21 00:34:11 -0700

    Added rand_num and isx86 functions

--- 1/prod-cons.c
+++ 1/prod-cons.c
@@ -1,18 +1,58 @@
 #include <stdio.h>
+#include <pthread.h>
+#include "mt19937ar.h"
 
 struct data {
-
+	int value;
+	int hold_time;
 };
 
+bool isx86();
 int rand_num(int, int);
 void consumer();
 void producer();
-void 
 
-int rand_num(int max, int min) {
+bool isx86() {
+	unsigned int eax = 0x01, ebc, ecx, edx;
+	
+	__asm__ __volatile__(
+			"cpuid;"
+			: "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx)
+			: "a"(eax)
+			);
 
+	if (ecx & 0x40000000) { return true; }
+	
+	return false;
 }
 
-int main() {
+int rand_num(int max, int min) {
+	int result = 0;
 
+	if (isx86()) {
+		__asm__ __volatile__(
+				"rdrand %0":"=r"(result)
+				);
+	}
+
+	result = abs(result % (max-min));
+	if (result < min) {
+		return min;
+	}
+
+	return result;
+}
+
+int main(int argc, char* argv[]) {
+	int threads, i;
+
+	if (argc < 1) {
+		printf("Usage: ./a.out [number of threads]");
+	}
+	threads = atoi(argv[1]);
+
+	
+
+	pthread_t consumer_process;
+	pthread_create(&consumer_process, NULL, (void *)consumer, (void *)NULL);
 }
